048c86ad81d21380d787fc03cd0859c6419c789e
caponewang <39684537+caponethedecapitator@users.noreply.github.com>
2022-11-05 20:00:51 -0500

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -6,5 +6,5 @@ INSERT INTO Customer VALUES('C14527', 'Jim', NULL, 'Nguyen', '2012-01-30', '5734
 
 #Employee
 SELECT * FROM Employee;
-INSERT INTO Employee VALUES('E331', 'John', 'K.', 'White', '130 Hass Street', 'M', '1995-03-10', '2022-12-15', NULL);
+INSERT INTO Employee VALUES('E331', 'John', 'K.', 'White', '130 Hass Street', 'M', '1995-03-10', '2020-12-15', NULL);
 
