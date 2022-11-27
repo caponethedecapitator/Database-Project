7e810e3b179b685906bc5e4acdac8ddb4b594d38
Ouyang <oxu@utdallas.edu>
2022-11-27 11:53:42 -0600

    Update Data.sql

---
+++ Data.sql
@@ -0,0 +1,7 @@
+#Employee
+INSERT INTO Employee VALUES('E331', 'John', 'K.', 'White', '130 Hass Street', 'M', '1995-03-10', '2020-12-15', NULL);
+INSERT INTO Employee VALUES('E178', 'Leo', Null, 'Huffman', '221 Hass Street', 'M', '1992-05-11', '2020-12-15', NULL);
+Delete from Employee where Employee_ID = 'E178';
+
+
+SELECT * FROM Employee;
