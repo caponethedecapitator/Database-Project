ec363452f04ff6477fc1cfa515377087e6e7c013
Ouyang <oxu@utdallas.edu>
2022-12-02 10:13:38 -0600

    Update Draft.sh

--- Draft.sh
+++ Draft.sh
@@ -1,31 +1,38 @@
-grep DESCRIBE EasyDelivery.sql 
-grep DROP EasyDelivery.sql 
+grep DESCRIBE EasyDelivery.sql
 
-DROP DATABASE EasyDelivery;
-DROP TABLE Customer cascade;
-DROP TABLE DeliveryAddress;
-DROP TABLE PremiumMember;
-DROP TABLE Employee;
-DROP TABLE PhoneNumber;
-DROP TABLE OrdinaryCustomer;
-DROP TABLE SilverMember;
-DROP TABLE Staff;
-DROP TABLE AreaManager;
-DROP TABLE Deliverer;
-DROP TABLE MemberCard;
-DROP TABLE Vehicle;
-DROP TABLE MealPass;
-DROP TABLE Shop;
-DROP TABLE OpeningTime;
-DROP TABLE ClosingTime;
-DROP TABLE MakeContract;
-DROP TABLE Promotion;
-DROP TABLE Comment;
-DROP TABLE Restaurant;
-DROP TABLE RestaurantType;
-DROP TABLE Supermarket;
-DROP TABLE Product;
-DROP TABLE Sell;
-DROP TABLE Orders CASCADE;
-DROP TABLE Payment CASCADE;
+grep DROP EasyDelivery.sql
+# DROP DATABASE EasyDelivery;
+# DROP TABLE Customer cascade;
+# DROP TABLE DeliveryAddress;
+# DROP TABLE PremiumMember;
+# DROP TABLE Employee;
+# DROP TABLE PhoneNumber;
+# DROP TABLE OrdinaryCustomer;
+# DROP TABLE SilverMember;
+# DROP TABLE Staff;
+# DROP TABLE AreaManager;
+# DROP TABLE Deliverer;
+# DROP TABLE MemberCard;
+# DROP TABLE Vehicle;
+# DROP TABLE MealPass;
+# DROP TABLE Shop;
+# DROP TABLE OpeningTime;
+# DROP TABLE ClosingTime;
+# DROP TABLE MakeContract;
+# DROP TABLE Promotion;
+# DROP TABLE Comment;
+# DROP TABLE Restaurant;
+# DROP TABLE RestaurantType;
+# DROP TABLE Supermarket;
+# DROP TABLE Product;
+# DROP TABLE Sell;
+# DROP TABLE Orders CASCADE;
+# DROP TABLE Payment CASCADE;
 
+
+grep DROP View.sql
+# DROP VIEW Annual_Top_3_Customers;
+# DROP VIEW Popular_Restaurant_Type;
+# DROP VIEW Potential_Silver_Member;
+# DROP VIEW Best_Area_Manager;
+# DROP VIEW Top_Restaurants;
