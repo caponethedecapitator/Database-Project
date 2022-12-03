cd62cbd5068a54ccbf18b39e119ff2f05bbd93a4
Ouyang <oxu@utdallas.edu>
2022-12-03 16:34:52 -0600

    change some data to accommodate the view

--- View.sql
+++ View.sql
@@ -1,18 +1,19 @@
 USE EasyDelivery;
--- DROP VIEW Annual_Top_3_Customers;
--- DROP VIEW Popular_Restaurant_Type;
--- DROP VIEW Potential_Silver_Member;
--- DROP VIEW Best_Area_Manager;
--- DROP VIEW Top_Restaurants;
+DROP VIEW Annual_Top_3_Customers;
+DROP VIEW Popular_Restaurant_Type;
+DROP VIEW Potential_Silver_Member;
+DROP VIEW Best_Area_Manager;
+DROP VIEW Top_Restaurants;
 CREATE VIEW Annual_Top_3_Customers AS
 SELECT Customer.First_Name AS Customer_First_Name, Customer.Last_Name AS Customer_Last_Name, SUM(Orders.Subtotal) AS Total_Order_Subtotal
 FROM ((Customer
 INNER JOIN Payment ON Customer.Customer_ID = Payment.Customer_ID)
 INNER JOIN Orders ON Orders.Order_ID = Payment.Order_ID)
 WHERE Payment.Payment_Time >= '2021-12-09'
+GROUP BY Customer.Customer_ID
 ORDER BY Total_Order_Subtotal DESC LIMIT 3;
 
-DESCRIBE Annual_Top_3_Customers;
+Select * from Annual_Top_3_Customers;
 -- DROP VIEW Annual_Top_3_Customers;
 
 CREATE VIEW Popular_Restaurant_Type AS
@@ -25,7 +26,7 @@ wHERE Payment.Payment_Time >= '2021-12-09'
 GROUP BY RestaurantType.Restaurant_Type
 ORDER BY Number_of_Orders DESC;
 
-DESCRIBE Popular_Restaurant_Type;
+Select * from Popular_Restaurant_Type;
 -- DROP VIEW Popular_Restaurant_Type;
 
 DROP VIEW Potential_Silver_Member;
@@ -40,8 +41,9 @@ INNER JOIN Payment ON Customer.Customer_ID = Payment.Customer_ID)
 WHERE Payment.Payment_Time >= '2020-11-09'
 GROUP BY Customer.Customer_ID
 HAVING Number_of_Orders > 3;
+
 Select * from Potential_Silver_Member;
--- DESCRIBE Potential_Silver_Member;
+-- Select * from Potential_Silver_Member;
 
 
 CREATE VIEW Best_Area_Manager AS
@@ -58,7 +60,7 @@ WHERE MakeContract.Contract_Start_Time >= '2021-12-09'
 GROUP BY AreaManager.Employee_ID
 ORDER BY Number_of_Contracts DESC;
 
-DESCRIBE Best_Area_Manager;
+Select * from Best_Area_Manager;
 -- DROP VIEW Best_Area_Manager;
 
 CREATE VIEW Top_Restaurants AS
@@ -73,5 +75,5 @@ WHERE Payment.Payment_Time >= '2022-11-09'
 GROUP BY RestaurantType.Restaurant_Type
 ORDER BY Number_of_Orders DESC;
 
-DESCRIBE Top_Restaurants;
--- DROP VIEW Top_Restaurants;
+Select * from Top_Restaurants;
+-- DROP VIEW Top_Restaurants; 
